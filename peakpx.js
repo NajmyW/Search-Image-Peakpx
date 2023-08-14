65812e87a74679c78662c2b406a157668f40fb3b
NajmyW <120731308+NajmyW@users.noreply.github.com>
2023-08-14 12:13:16 +0700

    Create peakpx.js

---
+++ peakpx.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+async function peakpx(q){
+  const { data } = await axios.get('https://www.peakpx.com/en/search?q='+q)
+  const $ = cheerio.load(data)
+  let img = []
+  $('figure > a').each(function(a,b) {
+    img.push($(b).attr('href'));
+  })
+  const random = img[Math.floor(Math.random() * img.length)]
+  const getHD = (await axios.get(random)).data
+  const $$ =  cheerio.load(getHD)
+  const imgHD = $$('#fig > img').attr('src')
+  return imgHD
+}
+// peakpx('loli')
